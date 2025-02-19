fcd4bc4b5de079d43f3ab810b7e1413ab9976942
lukasbuschdev <buschlukas175@gmail.com>
2025-02-19 17:23:05 -0600

    made basic setup for content containers

--- frontend/src/app/main/content/dashboard/dashboard.component.ts
+++ frontend/src/app/main/content/dashboard/dashboard.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
 
