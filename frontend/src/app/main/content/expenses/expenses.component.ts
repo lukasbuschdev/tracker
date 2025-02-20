5775df7b08f921469db2f9588bfd1643799d645d
lukasbuschdev <buschlukas175@gmail.com>
2025-02-20 12:35:14 -0600

    updated variables/styles.scss, implemented expenses content

--- frontend/src/app/main/content/expenses/expenses.component.ts
+++ frontend/src/app/main/content/expenses/expenses.component.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './expenses.component.scss'
 })
 export class ExpensesComponent {
+  isFilterVisible: boolean = false;
+  isDropdownOpened: boolean = false;
+  selectableBudgets: string[] = ['Income', 'Savings', 'Whatever'];
+  selectedBudget: string = 'Income';
 
+  toggleFilter(): void {
+    this.isFilterVisible = !this.isFilterVisible;
+  }
+
+  toggleBudgets(event: MouseEvent): void {
+    event.stopPropagation();
+    this.isDropdownOpened = !this.isDropdownOpened;
+  }
+
+  closeBudgtes(): void {
+    this.isDropdownOpened = false;
+  }
+
+  selectBudget(budget: string): void {
+    this.selectedBudget = budget;
+  }
 }
