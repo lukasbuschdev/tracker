b02aa692cee040db722886718f7e5e8205ec8221
lukasbuschdev <buschlukas175@gmail.com>
2025-03-15 20:02:15 -0600

    implemented delete functionality (budgets, categories, expenses), fixed minor design bug (dropdown)

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -4,7 +4,7 @@ import { Expense } from '../models/expense';
 import { Category } from '../models/category';
 import { DialogService } from './dialog.service';
 import { typeDialogData } from '../types/types';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,13 +15,13 @@ export class DataService {
   // currentUserId: string = '564eddf1-873b-44a6-91c0-1cc81defb1a2'; //ID Olaf
 
   private budgetsSubject = new BehaviorSubject<Budget[]>([]);
-  public budgets$: Observable<Budget[]> = this.budgetsSubject.asObservable();
+  // public budgets$: Observable<Budget[]> = this.budgetsSubject.asObservable();
 
   private categoriesSubject = new BehaviorSubject<Category[]>([]);
-  public categories$: Observable<Category[]> = this.categoriesSubject.asObservable();
+  // public categories$: Observable<Category[]> = this.categoriesSubject.asObservable();
 
   private expensesSubject = new BehaviorSubject<Expense[]>([]);
-  public expenses$: Observable<Expense[]> = this.expensesSubject.asObservable();
+  // public expenses$: Observable<Expense[]> = this.expensesSubject.asObservable();
 
 
   public get budgetsArray() {
@@ -38,7 +38,7 @@ export class DataService {
 
   selectedBudget: Budget | null = null;
   selectedCategory: string = 'Select category';
-  // currentAvailable: number = 0;
+
   clickedBudget: Budget | null = null; 
   clickedCategory: Category | null = null;
   clickedExpense: Expense | null = null;
@@ -79,7 +79,7 @@ export class DataService {
   }
 
 
-  public calculateCurrentAvailable(budget?: Budget): number {
+  public calculateCurrentAvailable(): number {
     if(!this.selectedBudget) return 0;
     const allExpenses = this.expensesSubject.value.map(expense => expense.amount);
     const expensesAmount = allExpenses.reduce((acc, curr) => acc + curr, 0);
@@ -120,7 +120,7 @@ export class DataService {
 
     const category = {
       name: dialogData.name,
-      budgetId: this.selectedBudget?.id,
+      budgetId: this.selectedBudget.id,
       amount: dialogData.amount,
       used: 0,
       recreate: false
@@ -136,7 +136,7 @@ export class DataService {
 
     const expense = {
       name: dialogData.name,
-      budgetId: this.selectedBudget?.id,
+      budgetId: this.selectedBudget.id,
       amount: dialogData.amount,
       category: this.selectedCategory,
       recreate: false
@@ -146,12 +146,13 @@ export class DataService {
     const currentExpenses = this.expensesSubject.value;
     this.expensesSubject.next([...currentExpenses, newExpense]);
 
-    this.updateUsed(dialogData.amount);
+    this.updateUsed();
   }
 
-  private async updateUsed(amount: number): Promise<void> {
+  private async updateUsed(): Promise<void> {
     if(!this.selectedBudget) return;
-    const updatedUsed = this.selectedBudget.used + amount;
+
+    let updatedUsed = this.selectedBudget.amount - this.calculateCurrentAvailable();
 
     const budgetUsed = {
       used: updatedUsed
@@ -182,13 +183,13 @@ export class DataService {
   }
 
   private async editBudget(dialogData: typeDialogData): Promise<void> {
+    if(!this.clickedBudget) return;
+    
     const budgetData = {
       name: dialogData.name,
       amount: dialogData.amount,
       recreate: false
     }
-
-    if(!this.clickedBudget) return;
 
     const editedBudget = await Budget.patch(this.clickedBudget.id, budgetData);
     const updatedBudgets = this.budgetsSubject.value.map(budget => {
@@ -202,15 +203,15 @@ export class DataService {
   }
 
   private async editCategory(dialogData: typeDialogData): Promise<void> {
+    if(!this.clickedCategory) return;
+    
     const categoryData = {
       name: dialogData.name,
       amount: dialogData.amount,
       recreate: false
     }
 
-    if(!this.clickedCategory) return;
     const clickedCategoryName = this.clickedCategory.name;
-
     const editedCategory = await Category.patch(this.clickedCategory.id, categoryData);
     const updatedCategories = this.categoriesSubject.value.map(category => {
       return category.id === editedCategory.id ? { ...category, ...editedCategory } : category;
@@ -241,14 +242,15 @@ export class DataService {
   }
 
   private async editExpense(dialogData: typeDialogData): Promise<void> {
+    if(!this.clickedExpense) return;
+    this.selectedCategory = this.dialog.category;
+    
     const expenseData = {
       name: dialogData.name,
       amount: dialogData.amount,
       category: this.selectedCategory,
       recreate: false
     }
-
-    if(!this.clickedExpense) return;
 
     const editedExpense = await Expense.patch(this.clickedExpense?.id, expenseData);
     const updatedExpenses = this.expensesSubject.value.map(expense => {
@@ -271,7 +273,64 @@ export class DataService {
 
   // // DELETE
 
-  deleteData() {
+  public deleteData() {
+    if(this.dialog.type === 'budget') return this.deleteBudget(this.clickedBudget?.id);
+    if(this.dialog.type === 'category') return this.deleteCategory(this.clickedCategory);
+    return this.deleteExpense(this.clickedExpense);
+  }
 
+  private async deleteBudget(budgetId?: string): Promise<void> {
+    if(!budgetId) return;
+
+    await Budget.delete(budgetId);
+
+    const updatedBudgets = this.budgetsArray.filter(budget => budget.id !== budgetId);
+    this.budgetsSubject.next(updatedBudgets);
+
+    this.updateCategoriesAndExpenses(budgetId);
+    this.dialog.closeDialog();
+  }
+
+  private updateCategoriesAndExpenses(budgetId: string): void {
+    const updatedCategories = this.categoriesArray.filter(category => category.budgetId !== budgetId);
+    const updatedExpenses = this.expensesArray.filter(expense => expense.budgetId !== budgetId);
+
+    this.categoriesSubject.next(updatedCategories);
+    this.expensesSubject.next(updatedExpenses);
+  }
+
+  private async deleteCategory(selectedCategory: Category | null): Promise<void> {
+    if(!selectedCategory) return;
+
+    await Category.delete(selectedCategory.id);
+
+    const updatedCategories = this.categoriesArray.filter(category => category.id !== selectedCategory.id);
+    this.categoriesSubject.next(updatedCategories);
+
+    await this.deleteExpensesOfCategory(selectedCategory);
+    this.dialog.closeDialog();
+  }
+
+  private async deleteExpensesOfCategory(category: Category): Promise<void> {
+    const expensesOfCategory = this.expensesArray.filter(expense => expense.category === category.name);
+    const promises = expensesOfCategory.map(expense => Expense.delete(expense.id));
+    await Promise.all(promises);
+
+    const updatedExpenses = this.expensesArray.filter(expense => expense.category !== category.name);
+    this.expensesSubject.next(updatedExpenses);
+
+    this.updateUsed();
+  }
+
+  private async deleteExpense(selectedExpense: Expense | null): Promise<void> {
+    if(!selectedExpense) return;
+
+    await Expense.delete(selectedExpense.id);
+
+    const updatedExpenses = this.expensesArray.filter(expense => expense.id !== selectedExpense.id);
+    this.expensesSubject.next(updatedExpenses);
+
+    this.updateUsed();
+    this.dialog.closeDialog();
   }
 }
