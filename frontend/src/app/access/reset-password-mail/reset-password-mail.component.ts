b1da31684ec1a6518956f8560d838f55efb5edc5
lukasbuschdev <buschlukas175@gmail.com>
2025-03-26 11:43:35 -0600

    links fixed again (without www.)

--- frontend/src/app/access/reset-password-mail/reset-password-mail.component.ts
+++ frontend/src/app/access/reset-password-mail/reset-password-mail.component.ts
@@ -75,7 +75,7 @@ export class ResetPasswordMailComponent {
       to: email,
       name: name,
       email: email,
-      url: 'https://www.expensetracker.lukasbusch.dev/reset-password',
+      url: 'https://expensetracker.lukasbusch.dev/reset-password',
       verificationCode: verificationCode,
       lang: lang
     }).subscribe(response => {
